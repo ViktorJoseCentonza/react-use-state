10b75ee3798ff398bb9266cd10c4fbae29aa7cf4
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-03-25 14:20:17 +0100

    add toggle code for buttons

--- src/components/ButtonSelectionComp.jsx
+++ src/components/ButtonSelectionComp.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+
+// export default function ButtonSelectionComp(props) {
+//     cibst [button,setButtonActive] = useState(false);
+//     return (
+//         <>
+//             {props.ButtonData.map((object) => (
+
+//                 <button key={`buttonSelect-${object.id}`} className={object.isOn ? "enabled" : "disabled"} onClick={() => object.isOn = !object.isOn}>{object.title}</button>
+
+
+
+//             ))}
+//         </>
+//     )
+// }
+
+
+import { useState } from 'react';
 
 export default function ButtonSelectionComp(props) {
+    const [buttonStates, setButtonActive] = useState(
+        props.ButtonData.map(object => object.isOn)
+    );
+
+    const handleButtonClick = (index) => {
+        const updatedButtonStates = [...buttonStates];
+        updatedButtonStates[updatedButtonStates.indexOf(true)] = false;
+        updatedButtonStates[index] = !updatedButtonStates[index];
+        setButtonActive(updatedButtonStates);
+    };
+
     return (
         <>
-            {props.ButtonData.map((object) => (
-
-                <button key={`buttonSelect-${object.id}`} className={object.isOn ? "enabled" : "disabled"} onClick={() => object.isOn = !object.isOn}>{object.title}</button>
-
-
-
+            {props.ButtonData.map((object, index) => (
+                <button
+                    key={`buttonSelect-${object.id}`}
+                    className={buttonStates[index] ? "enabled" : "disabled"}
+                    onClick={() => handleButtonClick(index)}
+                >
+                    {object.title}
+                </button>
             ))}
         </>
-    )
+    );
 }
